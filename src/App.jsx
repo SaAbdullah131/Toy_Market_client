c06f6887faeaac96a4fca70850f8bab5f9f89975
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-05-21 02:27:47 +0600

    client side setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-2xl tex-black text-center'>Welcome to ToddlerWay</h1>
+       
+    </>
+  )
+}
+
+export default App
